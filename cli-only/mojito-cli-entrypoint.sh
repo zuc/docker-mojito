6e72844d6d2a766c9787ca84874efa9400c8854f
Michele Zuccala <michele@zuccala.com>
2018-06-06 17:45:28 +0200

    fix cli params not being passed to app binary

--- cli-only/mojito-cli-entrypoint.sh
+++ cli-only/mojito-cli-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 set -e
-cd /opt/mojito/conf/
+cd /opt/mojito
 
 # Env vars interpolation
 envsubst < application.properties.dist > application.properties
 rm application.properties.dist
 
 # Go!
-cd /opt/mojito/bin/
-exec java -jar cli.jar --spring.config.location=/opt/mojito/conf/application.properties $@
+mkdir -p /opt/mojito/workdir && cd /opt/mojito/workdir
+exec java -jar /opt/mojito/cli.jar $@
