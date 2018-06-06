691ee52d57aa938da275e742536051b17dc3bbdd
Michele Zuccala <michele@zuccala.com>
2018-06-06 17:32:10 +0200

    set different path for bins and configs to avoid unintended config push to mojito server

--- cli-only/mojito-cli-entrypoint.sh
+++ cli-only/mojito-cli-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 set -e
-cd /opt/mojito/
+cd /opt/mojito/conf/
 
 # Env vars interpolation
 envsubst < application.properties.dist > application.properties
 rm application.properties.dist
 
 # Go!
-exec java -jar cli.jar $@
+cd /opt/mojito/bin/
+exec java -jar cli.jar --spring.config.location=/opt/mojito/conf/application.properties $@
--- full/mojito-webapp-launcher.sh
+++ full/mojito-webapp-launcher.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 set -e
-cd /opt/mojito/
+cd /opt/mojito/conf/
 
 # Env vars interpolation
 envsubst < application.properties.dist > application.properties
 rm application.properties.dist
 
 # Go!
-exec java -XX:MaxPermSize=128m -Xmx1024m -jar webapp.jar
+cd /opt/mojito/bin/
+exec java -XX:MaxPermSize=128m -Xmx1024m -jar webapp.jar --spring.config.location=/opt/mojito/conf/application.properties
