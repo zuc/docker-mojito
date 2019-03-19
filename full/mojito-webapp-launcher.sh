e4f4db9833d12bf2748dc3be6cf6819e3b5905fd
Marco Vito Moscaritolo <mavimo@gmail.com>
2019-03-19 19:59:35 +0100

    various changes
     - Update mojito to 0.96
     - Use multistage build to speed-up builds

--- full/mojito-webapp-launcher.sh
+++ full/mojito-webapp-launcher.sh
@@ -3,9 +3,11 @@
 set -e
 cd /opt/mojito
 
-# Env vars interpolation
-envsubst < application.properties.dist > application.properties
-rm application.properties.dist
+# Env vars interpolation (if not already processed)
+if [ -f application.properties.dist ]; then
+    envsubst < application.properties.dist > application.properties
+    rm application.properties.dist
+fi
 
 # Go!
 mkdir -p /opt/mojito/workdir && cd /opt/mojito/workdir
